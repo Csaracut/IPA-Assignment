f1ff2cc1dfccfe42b29fbc517b54f8d2578fc94f
Christian Saracut <Christian.Saracut27@gmail.com>
2022-10-29 19:34:04 +0100

    task 2

---
+++ task 2.py
@@ -0,0 +1,13 @@
+import matplotlib.pyplot as plt
+from skimage.transform import resize
+from skimage import data, io, filters, feature
+from skimage.color import rgb2gray
+from skimage.util import random_noise
+
+
+def main():
+    pass
+
+
+if __name__ == "__main__":
+    main()
