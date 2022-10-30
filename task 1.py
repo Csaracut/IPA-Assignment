c2a2604e69f7477d4f16f14044ce0c5dc057f222
Christian Saracut <Christian.Saracut27@gmail.com>
2022-10-30 01:01:41 +0100

    task 1

--- task 1.py
+++ task 1.py
@@ -9,10 +9,10 @@ def graph(grayscale_resized, noisy_image, laplace_filter, canny_filter):
     '''Defines a function to plots the grayscale image and noisy image with and without laplace and canny filters applied'''
     fig, (ax1, ax2, ax3, ax4) = plt.subplots(1, 4, figsize=(8, 4))
 
-    ax1.imshow(grayscale_resized)
-    ax2.imshow(noisy_image)
-    ax3.imshow(laplace_filter)
-    ax4.imshow(canny_filter)
+    ax1.imshow(grayscale_resized, cmap=plt.cm.gray)
+    ax2.imshow(noisy_image, cmap=plt.cm.gray)
+    ax3.imshow(laplace_filter, cmap=plt.cm.gray)
+    ax4.imshow(canny_filter, cmap=plt.cm.gray)
     
     ax1.set_title('Grayscale Resized')
     ax2.set_title('Noisy image')
@@ -27,14 +27,18 @@ def graph(grayscale_resized, noisy_image, laplace_filter, canny_filter):
 def main():
     ''''Defines the main functioin which reads the image and converts to grayscale, resizes and applies laplace and canny filters
         Prints the min, max and mean values of the grayscale image'''
-    filename = 'facepic.jpg'
-    original = io.imread(filename)
+    image_file = 'facepic.jpg'
+    original = io.imread(image_file)
+    
     grayscale = rgb2gray(original)
     grayscale_resized = resize(grayscale, (512, 512))
+    
     noisy_image = random_noise(grayscale_resized, mode='gaussian')
     laplace_filter = filters.laplace(noisy_image)
     canny_filter = feature.canny(noisy_image, mode='reflect')
+    
     print(f"Min: {grayscale_resized.min()} \nMax: {grayscale_resized.max()}\nMean: {grayscale_resized.mean()}")
+    
     graph(grayscale_resized, noisy_image, laplace_filter, canny_filter)
     
 
