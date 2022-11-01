8ec9bf2a9f87a2eff81d272436dfd034fce99aaf
Christian Saracut <Christian.Saracut27@gmail.com>
2022-11-01 21:59:47 +0000

    task 2

--- task 2.py
+++ task 2.py
@@ -11,8 +11,10 @@ from skimage.feature import match_template, peak_local_max
 
 
 def graph(grayscale_image, grayscale_image_temp, highlight, local_max):
-    '''defines function that plots the input image and template image
-       plots a highlighter around the target'''
+    '''defines function that plots a graph
+       shows the grayscale template image and grayscale input image
+       plots a highlighter around any instance of the template image inside the input image
+       plots peak local maxima for all instances of the template image'''
     fig, (ax1, ax2, ax3) = plt.subplots(1, 3, figsize=(8, 4))
 
     ax1.imshow(grayscale_image_temp, cmap=plt.cm.gray)
@@ -24,7 +26,7 @@ def graph(grayscale_image, grayscale_image_temp, highlight, local_max):
     ax1.set_title('template_tie')
     ax2.set_title('tie')
     ax3.set_title('local maxima')
-        
+    
     ax1.set_axis_off()
     ax2.set_axis_off()
     ax3.set_axis_off()
@@ -39,8 +41,9 @@ def graph(grayscale_image, grayscale_image_temp, highlight, local_max):
 def main():
     '''defines function which reads in the image template and input image
        converts images to grayscale
-       tries to match the template image to the input image and returns a normalised value
-       creates a variable for highlighting the target'''
+       matches the template image to the input image using correlation coefficients
+       creates a highlight around any instance of the template image
+       calculates the peak local maximas to find all instances of the template image'''
     image_file = 'tie.jpg'
     image_template_file = 'template_tie.jpg'
     image = io.imread(image_file)
@@ -50,10 +53,10 @@ def main():
     grayscale_image_temp = rgb2gray(image_template)
     image_float = img_as_float(grayscale_image)
     
-    result = match_template(grayscale_image, grayscale_image_temp)
+    result = np.array(match_template(grayscale_image, grayscale_image_temp))
     pos = np.unravel_index(np.argmax(result), result.shape)
     x, y = pos[::-1]
-    local_max = peak_local_max(image_float, min_distance=35)
+    local_max = peak_local_max(image_float, min_distance=1, threshold_rel=0.5)
     
     wtie, htie = grayscale_image_temp.shape
     highlight = plt.Rectangle((x, y), wtie, htie, edgecolor='r', facecolor='none')
