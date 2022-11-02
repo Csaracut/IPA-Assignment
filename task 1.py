aeea06b5b70bca100887d82e647ab8125a614057
Christian Saracut <Christian.Saracut27@gmail.com>
2022-11-02 19:44:41 +0000

    task 1

--- task 1.py
+++ task 1.py
@@ -1,6 +1,6 @@
 import matplotlib.pyplot as plt
 from skimage.transform import resize
-from skimage import data, io, filters, feature
+from skimage import io, filters, feature
 from skimage.color import rgb2gray
 from skimage.util import random_noise
 
