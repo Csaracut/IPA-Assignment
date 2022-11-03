615f5758fc7793ff0cee7a5dc96400c205e95214
Christian Saracut <Christian.Saracut27@gmail.com>
2022-11-03 11:53:09 +0000

    task 3

--- task 3.py
+++ task 3.py
@@ -6,10 +6,10 @@ from skimage.color import rgb2gray, label2rgb
 from skimage.filters import threshold_otsu
 from skimage.segmentation import clear_border
 from skimage.measure import label, regionprops
-from skimage.morphology import closing, square
+from skimage.morphology import closing
 
 
-def graph(overlay, object):
+def graph(overlay, label_object, min_area):
     fig, ax1 = plt.subplots(figsize=(10, 6))
 
     ax1.imshow(overlay)
@@ -18,17 +18,23 @@ def graph(overlay, object):
     
     ax1.set_axis_off()
 
-    for region in regionprops(object):
-        if region.area >= 50:
+    for region in regionprops(label_object):
+        if region.area >= 50 and region.area != min_area:
             min_y, min_x, max_y, max_x = region.bbox
             
-            rect = plt.Rectangle((min_x, min_y), max_x - min_x, max_y - min_y, fill=False, edgecolor='red')
+            highlight = plt.Rectangle((min_x, min_y), max_x - min_x, max_y - min_y, fill=False, edgecolor='red')
             
-            ax1.add_patch(rect)
-
+            ax1.add_patch(highlight)
+        elif region.area == min_area:
+            min_y, min_x, max_y, max_x = region.bbox
+            
+            highlight_smallest_raisin = plt.Rectangle((min_x, min_y), max_x - min_x, max_y - min_y, fill=False, edgecolor='blue')
+            
+            ax1.add_patch(highlight_smallest_raisin)
+            
     plt.tight_layout()
     plt.show()
-
+    
 
 def main():
     image_file = 'raisins.jpg'
@@ -41,11 +47,26 @@ def main():
     
     clear_objects= clear_border(border_objects)
     
-    object = label(clear_objects)
+    label_object = label(clear_objects)
     
-    overlay = label2rgb(object, image=grayscale_image, bg_label=0)
+    overlay = label2rgb(label_object, image=grayscale_image, alpha=0)
     
-    graph(overlay, object)
+    areas = np.array([])
+    count = 0
+    centroids = np.array([])
+    for region in regionprops(label_object):
+        if region.area >= 50:
+            centroids = np.append(centroids, region.centroid)
+            areas = np.append(areas, region.area)
+            count += 1
+    
+    min_area = np.sort(areas)[0]
+    min_centroid = np.sort(centroids)[0]
+
+    print(f"Number of raisins: {count}")
+    print(f"Area of smallest raisin: {min_area}\nCentroid of smallest raisin: {min_centroid}")
+     
+    graph(overlay, label_object, min_area)
 
 
 if __name__ == "__main__":
